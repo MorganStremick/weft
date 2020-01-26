029c31d83c10a31239a448e290386d109508b1e3
MorganStremick <sm60@hawaii.edu>
2020-01-25 18:04:02 -1000

    Completed placeholder Predicate component

--- weft/imports/ui/components/Layer.jsx
+++ weft/imports/ui/components/Layer.jsx
@@ -1,13 +1,63 @@
 import React, { Component } from 'react';
-import Draggable from './Draggable';
 import Predicate from './Predicate';
+import styled from 'styled-components';
+import { _ } from 'underscore';
 
 export default class Layer extends Component {
+    constructor(props) {
+        super(props);
+        const predicates = this.getPredicates();
+        let left = {};
+        let top = {};
+        _.each(predicates, (val, i) => { left[val._id] = 40 * i; top[val._id] = 40 * i; });
+
+        this.state = {
+            predicateIDs: _.pluck(predicates, '_id'),
+            left,
+            top,
+            selected: null,
+        };
+
+        this.handleMouseDown.bind(this);
+        this.handleMouseMove.bind(this);
+        this.handleMouseUp.bind(this);
+    }
+
+    renderPredicate(predicateID) {
+        return (
+            <Predicate key={predicateID}
+                       left={this.state.left[predicateID]}
+                       top={this.state.top[predicateID]}
+                       onMouseDown={(e) => this.handleMouseDown(predicateID, e)}
+            />
+        );
+    }
+
+    handleMouseMove() {
+        return;
+    }
+
+    handleMouseDown() {
+        return;
+    }
+
+    handleMouseUp() {
+        return;
+    }
+
+    getPredicates() {
+        return [
+            { _id: 1 },
+            { _id: 2 },
+            { _id: 3 },
+        ];
+    }
+
     render() {
         return (
-            <Draggable>
-                <Predicate />
-            </Draggable>
+            <div>
+                {_.map(this.state.predicateIDs, (predicateID) => this.renderPredicate(predicateID))}
+            </div>
         );
     }
 }
--- weft/imports/ui/components/Predicate.jsx
+++ weft/imports/ui/components/Predicate.jsx
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 
 export default class Predicate extends Component {
     render() {
+         const style = {
+             width: '20px',
+             height: '20px',
+             backgroundColor: 'pink',
+             position: 'absolute',
+             top: `${this.props.top}px`,
+             left: `${this.props.left}px`,
+         }
+
         return (
-            <div>Hello!!!</div>
+            <div style={style}></div>
         );
     }
 }
